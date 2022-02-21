ccb0866fad7b5796ec30311c78fef5b6e7519a2b
sebastiandres <sebastiandres@gmail.com>
2022-02-21 13:17:36 -0300

    Updating streamlit template with multipages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,24 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-
-DISPLAY_TEXT = "Display Text"
-DISPLAY_DATA = "Display Data"
+import streamlit_book as stb
 
 # Set wide display
 st.set_page_config(layout="wide")
 
-# Side Bar
-## Create a page selector (radio or )
-page = st.sidebar.radio("Choose your page:", 
-                          [
-                            DISPLAY_TEXT, 
-                            DISPLAY_DATA, 
-                            ]
-                        ) 
-## Links
-links = [
-        "* [Streamlit Cheat Sheet](https://share.streamlit.io/daniellewisdl/streamlit-cheat-sheet/app.py)",
-        "* [Streamlit Gallery](https://streamlit.io/gallery)",
-        ]
-st.sidebar.markdown("Links:") 
-st.sidebar.markdown("\n".join(links)) 
-
-## Autor
-st.sidebar.markdown("Created by [sebastiandres](https://sebastiandres.xyz)") 
-
-if page == DISPLAY_TEXT:
-    from pages import display_text
-    display_text.display_page(DISPLAY_TEXT)
-elif page == DISPLAY_DATA:
-    from pages import display_data
-    display_data.display_page(DISPLAY_DATA)
+# Set multipage
+save_answers = False
+stb.set_library_config(menu_title="App Title",
+                       menu_icon="apple",
+                       options=[
+                                "Intro",   
+                                "Multitest", 
+                                ], 
+                       paths=[
+                                "pages/Intro.py", 
+                                "pages/00 Multitest", 
+                            ],
+                       icons=[
+                                "tree", 
+                                "code", 
+                            ],
+                       save_answers=save_answers,
+                       )
